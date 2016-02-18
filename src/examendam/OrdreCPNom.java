1f3df4635dafe84028ec94f0664e871c23a7249b
Enric <enricsayos77@gmail.com>
2016-02-18 17:53:07 +0100

    Merge origin/master
    Conflicts:
    	src/examendam/Cinema.java

---
+++ src/examendam/OrdreCPNom.java
@@ -0,0 +1,28 @@
+package examendam;
+
+import java.util.Comparator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author enric
+ */
+public class OrdreCPNom implements Comparator{
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        Espectador esp1 = (Espectador) o1;
+        Espectador esp2 = (Espectador) o2;
+        if (!esp1.getCp().equals(esp2.getCp())){
+            return esp1.getCp().compareTo(esp2.getCp());
+        }else{
+            return esp1.getCp().compareTo(esp2.g)
+        }
+    }
+    
+}
